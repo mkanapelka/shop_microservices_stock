cf67413945054179cdda8724cff3596911c4470e
mkanapelka <MKanapelka@piesoft.us>
2022-06-28 10:24:19 +0300

    register models

--- stock/admin.py
+++ stock/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 from stock.models import Product, Characteristic, Category
 
-admin.site.regicter(Product)
-admin.site.regicter(Category)
-admin.site.regicter(Characteristic)
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Characteristic)
